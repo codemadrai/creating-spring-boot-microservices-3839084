597ee43deab1dcf93b3cbb19d8cfba3dd23f9b08
codemadrai <codemadrai@gmail.com>
2026-04-28 22:16:30 +0000

    add configuration and new repository files for class

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,13 @@
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.explorecalijpa.model.Tour;
+
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+  
+
+
+
+
+}  /* TourRepository end */ 
