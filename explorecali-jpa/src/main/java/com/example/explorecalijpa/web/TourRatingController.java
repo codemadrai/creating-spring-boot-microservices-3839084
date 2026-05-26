7c2f8d8c813e8da66376fdb84d51aef5a7df27cd
codemadrai <codemadrai@gmail.com>
2026-05-26 23:02:23 +0000

    add stuff

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,14 +1,17 @@
 package com.example.explorecalijpa.web;
 
+import com.example.explorecalijpa.repo.TourRatingRepository;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,10 +30,12 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
+  private final TourRatingRepository tourRatingRepository;
   private TourRatingService tourRatingService;
 
-  public TourRatingController(TourRatingService tourRatingService) {
+  public TourRatingController(TourRatingService tourRatingService, TourRatingRepository tourRatingRepository) {
     this.tourRatingService = tourRatingService;
+    this.tourRatingRepository = tourRatingRepository;
   }
 
   /**
@@ -48,6 +53,12 @@ public class TourRatingController {
       return new RatingDto(rating);
   }
 
+  /**
+   * Get all ratings for a specified tour id...
+   * 
+   * @param tourId
+   * @return
+   */
   @GetMapping
   public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
     List<TourRating> tourRatings = tourRatingService.lookupRatings(tourId);
@@ -65,6 +76,39 @@ public class TourRatingController {
     return Map.of("average", tourRatingService.getAverageScore(tourId));
   }
 
+  /**
+   * Update with a put - here we get a tourId and request body of json 
+   *   that models a RatingDto..
+   * 
+   * @param tourId
+   * @param ratingDto
+   * @return
+   */
+  @PutMapping
+  public RatingDto updateWithPut(@PathVariable(value="tourId") int tourId,
+      @RequestBody @Valid RatingDto ratingDto) {
+    return new RatingDto(tourRatingService.update(tourId, 
+      ratingDto.getCustomerId(),
+      ratingDto.getScore(), 
+      ratingDto.getComment())); 
+  }
+
+
+  /**
+   * Delete a customers tour rating...
+   * 
+   * @param tourId
+   * @param customerId
+   */
+  @DeleteMapping("{customerId}")
+  public void delete(@PathVariable (value="tourId") int tourId, @PathVariable(value="customerId") int customerId) {
+    tourRatingService.delete(tourId, customerId);
+  }
+
+  /**
+   * Handles the NoSuchElementException with a 404 return error
+   * 
+   */
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public String return404(NoSuchElementException exception) {
